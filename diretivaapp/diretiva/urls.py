4f2c8d4862ebe1417ddb29a6cf4a812ffd6024d2
Jonatan Vianna <jonatan.vianna@gmail.com>
2017-09-25 22:41:38 -0300

    crud api

--- diretivaapp/diretiva/urls.py
+++ diretivaapp/diretiva/urls.py
@@ -1,5 +1,13 @@
-from django.conf.urls import url, include
-from . import views 
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
+
 urlpatterns = [
-    url(r'diretivas/$', views.diretiva_list)
+
+    url(r'diretivas/$', views.DiretivaList.as_view()),
+    url(r'diretivas/(?P<pk>[0-9]+)/$', views.DiretivaDetail.as_view()),
+    
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- diretivaapp/diretiva/views.py
+++ diretivaapp/diretiva/views.py
@@ -1,26 +1,115 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from .models import Diretiva
 from .serializers import DiretivaSerializer
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.decorators import api_view
+from rest_framework import renderers
 
 
-@csrf_exempt
-def diretiva_list(request):
-    if request.method == 'GET':
-        diretivas = Diretiva.objects.all()
-        serializer = DiretivaSerializer(diretivas, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class DiretivaList(generics.ListCreateAPIView):
+    queryset = Diretiva.objects.all()
+    serializer_class = DiretivaSerializer
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = DiretivaSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+
+class DiretivaDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Diretiva.objects.all()
+    serializer_class = DiretivaSerializer
 
 
 
+
+
+# FUNCTION BASED API
+
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# from .models import Diretiva
+# from .serializers import DiretivaSerializer
+#
+#
+# @api_view(['GET', 'POST'])
+# def diretiva_list(request, format=None):
+#     if request.method == 'GET':
+#         diretivas = Diretiva.objects.all()
+#         serializer = DiretivaSerializer(diretivas, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = DiretivaSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def diretiva_detail(request, pk, format=None):
+#     try:
+#         diretiva = Diretiva.objects.get(pk=pk)
+#     except Diretiva.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     if request.method == 'GET':
+#         serializer = DiretivaSerializer(diretiva)
+#         return Response(data=serializer.data)
+#
+#     elif request.method == 'PUT':
+#         serializer = DiretivaSerializer(diretiva, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     elif request.method == 'DELETE':
+#         diretiva.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+# OLD VERSION
+
+# from django.views.decorators.csrf import csrf_exempt
+# from rest_framework.renderers import JSONRenderer
+# from rest_framework.parsers import JSONParser
+# from .models import Diretiva
+# from .serializers import DiretivaSerializer
+#
+#
+# @csrf_exempt
+# def diretiva_list(request):
+#     if request.method == 'GET':
+#         diretivas = Diretiva.objects.all()
+#         serializer = DiretivaSerializer(diretivas, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+#
+#     elif request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = DiretivaSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)
+#
+#
+# @csrf_exempt
+# def diretiva_detail(request, pk):
+#     try:
+#         diretiva = Diretiva.objects.get(pk=pk)
+#     except Diretiva.DoesNotExist:
+#         return HttpResponse(status=404)
+#
+#     if request.method == 'GET':
+#         seriaizer = DiretivaSerializer(diretiva)
+#         return JsonResponse(seriaizer.data)
+#     elif request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         seriaizer = DiretivaSerializer(diretiva, data=data)
+#         if seriaizer.is_valid():
+#             seriaizer.save()
+#             return JsonResponse(seriaizer.data)
+#     elif request.method == 'DELETE':
+#         diretiva.delete()
+#         return HttpResponse(status=204)
